182ea50b47153a38ac7b9caab8e228caed418511
Tony <tonym@sb401k.com>
2021-10-20 15:22:20 -0700

    Renaming for clarity and better context

--- main.go
+++ main.go
@@ -13,16 +13,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Handler struct {
+type Context struct {
 	WorkingDir  string
 	Templates   template.HTMLTemplateInterface
 	PDF         pdf.PDFProviderInterface
 	Development bool
 }
 
-func (h *Handler) handler(w http.ResponseWriter, r *http.Request) {
+func (c *Context) handleRequest(w http.ResponseWriter, r *http.Request) {
 	j := json.NewDecoder(r.Body)
-	pdfModel := model.NewPDFModel(h.WorkingDir)
+	pdfModel := model.NewPDFModel(c.WorkingDir)
 	err := j.Decode(pdfModel)
 	pdfModel.FillMissing()
 
@@ -32,19 +32,19 @@ func (h *Handler) handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if h.Development {
+	if c.Development {
 		// Refresh the Template Each Test
-		h.Templates = template.NewHTMLTemplate()
+		c.Templates = template.NewHTMLTemplate()
 	}
 
-	t, err := h.Templates.Compile(pdfModel, "PDF")
+	t, err := c.Templates.Compile(pdfModel, "PDF")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Error creating purchase agreement template: " + err.Error()))
 		return
 	}
 
-	pdf, err := h.PDF.CreatePDF(t)
+	pdf, err := c.PDF.CreatePDF(t)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Error generating purchase agreement PDF: " + err.Error()))
@@ -58,7 +58,7 @@ func main() {
 	wd, _ := os.Getwd()
 	t := template.NewHTMLTemplate()
 	p := pdf.NewPDFProvider()
-	h := &Handler{
+	c := &Context{
 		PDF:         p,
 		Templates:   t,
 		WorkingDir:  wd,
@@ -70,7 +70,7 @@ func main() {
 		fmt.Fprintf(w, "Lets create some PDFs!\n")
 	})
 
-	r.HandleFunc("/pdf", h.handler).Methods("POST")
+	r.HandleFunc("/pdf", c.handleRequest).Methods("POST")
 
 	fmt.Println("Listening on port 3000")
 	log.Fatal(http.ListenAndServe("localhost:3000", r))
